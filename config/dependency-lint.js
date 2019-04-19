9ca8df89f2e743885cb5d2924ec696d25f46b11e
0xADADA <radams@salsify.com>
2019-04-19 15:22:04 -0400

    [RET-79] resolved dep lint issues

---
+++ config/dependency-lint.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = {
+  allowedVersions: {
+    // these versions are all compatable
+    'ember-cli-clipboard': '0.8.1 || 0.11.1',
+
+    // 2.1.0 introduced the `is-empty` helper
+    'ember-truth-helpers': '2.0.0 || 2.1.0',
+
+    // these versions are all compatable
+    'ember-assign-polyfill': '2.4.0 || 2.6.0',
+  },
+};
