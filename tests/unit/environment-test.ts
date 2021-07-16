f9d9ee004d6875532170375aaae44f38dea7456a
Dan Freeman <dfreeman@salsify.com>
2021-07-16 16:47:35 +0200

    Convert environment test to TS

--- tests/unit/environment-test.ts
+++ tests/unit/environment-test.ts
@@ -1,62 +1,62 @@
-import { set, get } from '@ember/object';
 import { module, test } from 'qunit';
+import { wrap, read, write } from 'ember-exclaim';
+import { A } from '@ember/array';
 import Binding from 'ember-exclaim/-private/binding';
 import HelperSpec from 'ember-exclaim/-private/helper-spec';
-import Environment, {
-  wrap,
-  resolvePath,
-} from 'ember-exclaim/-private/environment';
+import Environment, { resolvePath } from 'ember-exclaim/-private/environment';
 import { htmlSafe } from '@ember/string';
+import EnvironmentData from '../../addon/-private/environment/data';
+import EnvironmentArray from '../../addon/-private/environment/array';
 
 module('Unit | environment', function () {
   test('simple lookups', function (assert) {
     const env = new Environment({ foo: 'bar' });
-    assert.equal(get(env, 'foo'), 'bar');
+    assert.equal(read(env, 'foo'), 'bar');
 
-    set(env, 'foo', 'baz');
-    assert.equal(get(env, 'foo'), 'baz');
+    write(env, 'foo', 'baz');
+    assert.equal(read(env, 'foo'), 'baz');
   });
 
   test('simple array lookups', function (assert) {
     const env = new Environment({ foo: ['bar', 'baz'] });
-    assert.equal(get(env, 'foo.0'), 'bar');
-    assert.equal(get(env, 'foo.1'), 'baz');
+    assert.equal(read(env, 'foo.0'), 'bar');
+    assert.equal(read(env, 'foo.1'), 'baz');
   });
 
   test('set unknown property', function (assert) {
-    const env = new Environment({ foo: ['bar'] });
-    set(env, 'baz', 'bax');
-    assert.equal(get(env, 'baz'), 'bax');
-    assert.equal(get(env, 'foo.length'), 1);
-    set(env, 'foo.3', 'qux');
-    assert.equal(get(env, 'foo.3'), 'qux');
-    assert.equal(get(env, 'foo.length'), 4);
+    const env = new Environment({ foo: ['bar'], baz: '' });
+    write(env, 'baz', 'bax');
+    assert.equal(read(env, 'baz'), 'bax');
+    assert.equal(read(env, 'foo.length'), 1);
+    write(env, 'foo.3', 'qux');
+    assert.equal(read(env, 'foo.3'), 'qux');
+    assert.equal(read(env, 'foo.length'), 4);
   });
 
   test('array mutation', function (assert) {
     const foo = ['bar', 'baz'];
     const env = new Environment({ foo });
-    set(env, 'foo.1', 'oops');
+    write(env, 'foo.1', 'oops');
     assert.equal(foo[1], 'oops');
-    assert.equal(get(env, 'foo.1'), 'oops');
-    set(env, 'foo.0', 'oops again');
-    assert.equal(get(foo, 'firstObject'), 'oops again');
+    assert.equal(read(env, 'foo.1'), 'oops');
+    write(env, 'foo.0', 'oops again');
+    assert.equal(read(foo, 'firstObject'), 'oops again');
   });
 
   test('HTML-safe strings', function (assert) {
     const env = new Environment({ foo: htmlSafe('hello') });
-    assert.deepEqual(get(env, 'foo'), htmlSafe('hello'));
+    assert.deepEqual(read(env, 'foo'), htmlSafe('hello'));
   });
 
   test('simple binding resolution', function (assert) {
     const env = new Environment({ foo: 'bar', baz: new Binding('foo') });
-    assert.equal(get(env, 'baz'), 'bar');
+    assert.equal(read(env, 'baz'), 'bar');
 
-    set(env, 'foo', 'qux');
-    assert.equal(get(env, 'baz'), 'qux');
+    write(env, 'foo', 'qux');
+    assert.equal(read(env, 'baz'), 'qux');
 
-    set(env, 'baz', 'fizz');
-    assert.equal(get(env, 'foo'), 'fizz');
+    write(env, 'baz', 'fizz');
+    assert.equal(read(env, 'foo'), 'fizz');
   });
 
   test('complex binding resolution', function (assert) {
@@ -65,60 +65,65 @@ module('Unit | environment', function () {
       other: new Binding('x'),
       stillMore: new Binding('other.z'),
     });
-    assert.equal(get(env, 'stillMore'), 'bar');
+    assert.equal(read(env, 'stillMore'), 'bar');
     assert.equal(resolvePath(env, 'x.y'), 'x.y');
     assert.equal(resolvePath(env, 'other'), 'x');
     assert.equal(resolvePath(env, 'stillMore'), 'x.z');
 
-    set(env, 'x.z', 123);
-    assert.equal(get(env, 'other.z'), 123);
-    assert.equal(get(env, 'stillMore'), 123);
+    write(env, 'x.z', '123');
+    assert.equal(read(env, 'other.z'), '123');
+    assert.equal(read(env, 'stillMore'), '123');
 
-    set(env, 'other.z', 234);
-    assert.equal(get(env, 'x.z'), 234);
-    assert.equal(get(env, 'stillMore'), 234);
+    write(env, 'other.z', 234);
+    assert.equal(read(env, 'x.z'), 234);
+    assert.equal(read(env, 'stillMore'), 234);
 
-    set(env, 'stillMore', 345);
-    assert.equal(get(env, 'x.z'), 345);
-    assert.equal(get(env, 'other.z'), 345);
+    write(env, 'stillMore', 345);
+    assert.equal(read(env, 'x.z'), 345);
+    assert.equal(read(env, 'other.z'), 345);
   });
 
   test('value reference resolution', function (assert) {
     const env = new Environment({ foo: 'bar' });
     const value = wrap({ key: new Binding('foo') }, env);
-    assert.equal(get(value, 'key'), 'bar');
+    assert.equal(read(value, 'key'), 'bar');
     assert.equal(resolvePath(value, 'key'), 'foo');
 
-    set(value, 'key', 'ok');
-    assert.equal(get(env, 'foo'), 'ok');
+    write(value, 'key', 'ok');
+    assert.equal(read(env, 'foo'), 'ok');
   });
 
   test('chained reference resolution', function (assert) {
     const env = new Environment({ foo: 'bar' });
     const value = wrap({ key: { child: new Binding('foo') } }, env);
-    const subvalue = get(value, 'key');
-    assert.equal(get(subvalue, 'child'), 'bar');
+    const subvalue = read(value, 'key');
+    assert.equal(read(subvalue, 'child'), 'bar');
 
     assert.equal(resolvePath(value, 'key'), undefined);
     assert.equal(resolvePath(value, 'key.child'), 'foo');
     assert.equal(resolvePath(subvalue, 'child'), 'foo');
 
-    set(subvalue, 'child', 321);
-    assert.equal(get(value, 'key.child'), 321);
-    assert.equal(get(env, 'foo'), 321);
+    write(subvalue, 'child', 321);
+    assert.equal(read(value, 'key.child'), 321);
+    assert.equal(read(env, 'foo'), 321);
 
-    set(env, 'foo', 999);
-    assert.equal(get(subvalue, 'child'), 999);
+    write(env, 'foo', '999');
+    assert.equal(read(subvalue, 'child'), '999');
   });
 
   test('array values', function (assert) {
-    const env = new Environment({ foo: 2, array: [1, new Binding('foo'), 3] });
-    const ref = wrap({ array: new Binding('array') }, env);
-    assert.deepEqual(get(env, 'array').toArray(), [1, 2, 3]);
-    assert.deepEqual(get(ref, 'array').toArray(), [1, 2, 3]);
+    const env = new Environment({
+      foo: 2,
+      array: A([1, new Binding('foo'), 3]),
+    });
 
-    const envArray = get(env, 'array');
-    const refArray = get(ref, 'array');
+    const ref = wrap({ array: new Binding('array') }, env);
+    const envArray = read(env, 'array');
+    const refArray = read(ref, 'array') as EnvironmentArray<unknown>;
+
+    assert.deepEqual(envArray.toArray(), [1, 2, 3]);
+    assert.deepEqual(refArray.toArray(), [1, 2, 3]);
+
     assert.equal(envArray.objectAt(1), 2);
     assert.equal(refArray.objectAt(1), 2);
 
@@ -134,48 +139,53 @@ module('Unit | environment', function () {
     assert.deepEqual(envArray.toArray(), [100, 3, 4]);
     assert.deepEqual(refArray.toArray(), [100, 3, 4]);
 
-    set(env, 'foo', 0);
+    write(env, 'foo', 0);
     assert.deepEqual(envArray.toArray(), [0, 3, 4]);
     assert.deepEqual(refArray.toArray(), [0, 3, 4]);
   });
 
   test('environment extension', function (assert) {
-    const base = new Environment({ a: 'bar' });
+    const base = new Environment({ a: 'bar' } as {
+      a: string;
+      c?: string;
+      d?: string;
+    });
+
     const ext1 = base.extend({ b: 'qux' });
     const ext2 = base.extend({ a: 'newbar' });
 
-    assert.equal(get(base, 'a'), 'bar');
-    assert.equal(get(base, 'b'), undefined);
+    assert.equal(read(base, 'a'), 'bar');
+    assert.equal(read(base, 'b'), undefined);
 
-    assert.equal(get(ext1, 'a'), 'bar');
-    assert.equal(get(ext1, 'b'), 'qux');
+    assert.equal(read(ext1, 'a'), 'bar');
+    assert.equal(read(ext1, 'b'), 'qux');
 
-    assert.equal(get(ext2, 'a'), 'newbar');
-    assert.equal(get(ext2, 'b'), undefined);
+    assert.equal(read(ext2, 'a'), 'newbar');
+    assert.equal(read(ext2, 'b'), undefined);
 
     // Writing to an inherited key updates at the level it was inherited from
-    set(ext1, 'a', 'x');
-    assert.equal(get(base, 'a'), 'x');
-    assert.equal(get(ext1, 'a'), 'x');
-    assert.equal(get(ext2, 'a'), 'newbar');
+    write(ext1, 'a', 'x');
+    assert.equal(read(base, 'a'), 'x');
+    assert.equal(read(ext1, 'a'), 'x');
+    assert.equal(read(ext2, 'a'), 'newbar');
 
     // Writing to an overridden key updates at the level of the override
-    set(ext2, 'a', 'y');
-    assert.equal(get(base, 'a'), 'x');
-    assert.equal(get(ext1, 'a'), 'x');
-    assert.equal(get(ext2, 'a'), 'y');
+    write(ext2, 'a', 'y');
+    assert.equal(read(base, 'a'), 'x');
+    assert.equal(read(ext1, 'a'), 'x');
+    assert.equal(read(ext2, 'a'), 'y');
 
     // Writing a previously-nonexistent key sets it in the leaf environment
-    set(ext1, 'c', 'newvalue');
-    assert.equal(get(base, 'c'), undefined);
-    assert.equal(get(ext1, 'c'), 'newvalue');
-    assert.equal(get(ext2, 'c'), undefined);
+    write(ext1, 'c', 'newvalue');
+    assert.equal(read(base, 'c'), undefined);
+    assert.equal(read(ext1, 'c'), 'newvalue');
+    assert.equal(read(ext2, 'c'), undefined);
 
     // Writing a previously-nonexistent key in the base propagates down
-    set(base, 'd', 'everywhere');
-    assert.equal(get(base, 'd'), 'everywhere');
-    assert.equal(get(ext1, 'd'), 'everywhere');
-    assert.equal(get(ext2, 'd'), 'everywhere');
+    write(base, 'd', 'everywhere');
+    assert.equal(read(base, 'd'), 'everywhere');
+    assert.equal(read(ext1, 'd'), 'everywhere');
+    assert.equal(read(ext2, 'd'), 'everywhere');
   });
 
   test('path resolution', function (assert) {
@@ -211,12 +221,12 @@ module('Unit | environment', function () {
       'nested.super.extra.nested'
     );
 
-    const nested = get(env, 'nested');
+    const nested = read(env, 'nested');
     assert.equal(resolvePath(nested, 'nonexistent'), 'nested.nonexistent');
     assert.equal(resolvePath(nested, 'ownKey'), 'nested.ownKey');
     assert.equal(resolvePath(nested, 'pointsToRoot'), 'root');
 
-    const array = get(env, 'array');
+    const array = read(env, 'array');
     assert.equal(resolvePath(array, 'firstObject.pointsToRoot'), 'root');
     assert.equal(resolvePath(array, 'firstObject.ownKey'), 'nested.ownKey');
     assert.equal(resolvePath(array, 'lastObject.ownKey'), 'array.1.ownKey');
@@ -245,7 +255,7 @@ module('Unit | environment', function () {
     const subenv = env.extend({
       subValue: 'hello',
       upReference: new Binding('nested.ownKey'),
-      recycled: get(env, 'nested.super'),
+      recycled: read(env, 'nested.super'),
     });
 
     assert.equal(resolvePath(subenv, 'upReference'), 'nested.ownKey');
@@ -276,7 +286,7 @@ module('Unit | environment', function () {
       path: 'foo',
     });
 
-    const value = get(env, 'deep');
+    const value = read(env, 'deep');
     assert.deepEqual(env.metaForField(value, 'own'), {
       tag: 'so meta',
       path: 'deep.own',
@@ -289,37 +299,46 @@ module('Unit | environment', function () {
 });
 
 test('helper invocation', function (assert) {
+  type WordConfig = EnvironmentData<{ word: string }>;
+
   const env = new Environment({
     foo: 'bar',
-    shouty: new HelperSpec((config) => get(config, 'word').toUpperCase(), {
-      word: new Binding('foo'),
-    }),
+    shouty: new HelperSpec(
+      (config: WordConfig) => read(config, 'word').toUpperCase(),
+      {
+        word: new Binding('foo'),
+      }
+    ),
     array: [
       1,
       2,
-      new HelperSpec((config) => get(config, 'word.length'), {
+      new HelperSpec((config) => read(config, 'word.length'), {
         word: new Binding('shouty'),
       }),
     ],
-    nested: new HelperSpec((config) => get(config, 'word').toUpperCase(), {
-      word: new HelperSpec(
-        (config) => get(config, 'word').split('').reverse().join(''),
-        {
-          word: new Binding('foo'),
-        }
-      ),
-    }),
+    nested: new HelperSpec(
+      (config: WordConfig) => read(config, 'word').toUpperCase(),
+      {
+        word: new HelperSpec(
+          (config: WordConfig) =>
+            read(config, 'word').split('').reverse().join(''),
+          {
+            word: new Binding('foo'),
+          }
+        ),
+      }
+    ),
   });
 
-  assert.equal(get(env, 'foo'), 'bar');
-  assert.equal(get(env, 'shouty'), 'BAR');
-  assert.equal(get(env, 'nested'), 'RAB');
-  assert.deepEqual(get(env, 'array').toArray(), [1, 2, 3]);
+  assert.equal(read(env, 'foo'), 'bar');
+  assert.equal(read(env, 'shouty'), 'BAR');
+  assert.equal(read(env, 'nested'), 'RAB');
+  assert.deepEqual(read(env, 'array').toArray(), [1, 2, 3]);
 
-  set(env, 'foo', 'ok');
+  write(env, 'foo', 'ok');
 
-  assert.equal(get(env, 'foo'), 'ok');
-  assert.equal(get(env, 'shouty'), 'OK');
-  assert.equal(get(env, 'nested'), 'KO');
-  assert.deepEqual(get(env, 'array').toArray(), [1, 2, 2]);
+  assert.equal(read(env, 'foo'), 'ok');
+  assert.equal(read(env, 'shouty'), 'OK');
+  assert.equal(read(env, 'nested'), 'KO');
+  assert.deepEqual(read(env, 'array').toArray(), [1, 2, 2]);
 });
