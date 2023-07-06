b9927a21cc6b8f863da6bc6b0d3599d705956b1c
Dan Freeman <dfreeman@salsify.com>
2023-07-06 16:51:07 +0200

    Upgrade to `ember-ace@3` for recent Ember compat

--- playground-app/app/components/json-editor/component.js
+++ playground-app/app/components/json-editor/component.js
@@ -1,17 +1,26 @@
-import { get } from '@ember/object';
+import { get, action } from '@ember/object';
 import Component from '@ember/component';
+
+import 'ace-builds/src-noconflict/ace';
+import 'ace-builds/src-noconflict/theme-chrome';
+import 'ace-builds/src-noconflict/mode-json';
+
+import * as jsonWorkerUrl from 'ace-builds/src-noconflict/worker-json?resource';
+import { config } from 'ace-builds';
+
+config.setModuleUrl('ace/mode/json_worker', jsonWorkerUrl);
 
 export default Component.extend({
   tagName: '',
 
   string: null,
 
-  valueChanged(newString) {
+  valueChanged: action(function (newString) {
     try {
       JSON.parse(newString);
       get(this, 'onChange')(newString);
     } catch (error) {
       // Ignore, just don't update
     }
-  },
+  }),
 });
--- playground-app/ember-cli-build.js
+++ playground-app/ember-cli-build.js
@@ -6,15 +6,11 @@ module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     autoImport: {
       watchDependencies: ['ember-exclaim'],
-    },
-    ace: {
-      themes: ['chrome'],
-      modes: ['json'],
-      workers: ['json'],
-      workerPath:
-        EmberApp.env() === 'production'
-          ? '/ember-exclaim/assets/ace'
-          : undefined,
+      webpack: {
+        module: {
+          rules: [{ resourceQuery: /resource/, type: 'asset/resource' }],
+        },
+      },
     },
   });
 
