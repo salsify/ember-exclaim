178c8d604c14a4b74557e88d7ea9c65d9d1ea77f
Dan Freeman <dfreeman@salsify.com>
2024-03-07 17:04:53 +0000

    Expose `ComponentSpec` and `HelperSpec` as public imports

--- addon/index.js
+++ addon/index.js
@@ -1,1 +1,3 @@
 export { wrap, unwrap } from 'ember-exclaim/-private/environment';
+export { default as ComponentSpec } from 'ember-exclaim/-private/component-spec';
+export { default as HelperSpec } from 'ember-exclaim/-private/helper-spec';
