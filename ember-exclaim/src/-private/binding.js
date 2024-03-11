b6de487221e3eef37bde7cdf200eb6c424ca7f52
Dan Freeman <dfreeman@salsify.com>
2024-03-11 16:07:20 +0000

    Move "parsed spec" stuff into `ui-spec.js`

--- ember-exclaim/src/-private/binding.js
+++
@@ -1,5 +0,0 @@
-export default class Binding {
-  constructor(path) {
-    this.path = path.split('.');
-  }
-}
--- ember-exclaim/src/index.js
+++ ember-exclaim/src/index.js
@@ -1,1 +1,2 @@
+export { ComponentSpec, HelperSpec } from './-private/ui-spec.js';
 export { wrap, unwrap } from './-private/environment';
