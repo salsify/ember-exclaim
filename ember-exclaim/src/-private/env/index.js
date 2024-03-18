321beea7dfefdca6b712124f25d18afac648326c
Dan Freeman <dfreeman@salsify.com>
2024-03-18 14:19:32 +0000

    Use a consistent technique for global storage

--- ember-exclaim/src/-private/env/index.js
+++ ember-exclaim/src/-private/env/index.js
@@ -1,15 +1,18 @@
-const EnvInternals = Symbol.for('env-internals');
+// This fun bit of line noise is a workaround for
+// https://github.com/embroider-build/ember-auto-import/issues/503#issuecomment-1064405138
+const envInternals = (globalThis[Symbol.for('exclaim-env-internals')] ??=
+  new WeakMap());
 
 export function makeEnv(data, onChange, { bind, extend }) {
   return setInternals(data, { onChange, bind, extend });
 }
 
 export function isEnv(data) {
-  return EnvInternals in data;
+  return envInternals.has(data);
 }
 
 export function extendEnv(env, newBindings) {
-  const internals = env[EnvInternals];
+  const internals = envInternals.get(env);
   const newEnv = internals.extend(env, newBindings);
   const onChange = (key) => {
     // We only want to propagate `onChange` if it's to a key in the
@@ -24,19 +27,17 @@ export function extendEnv(env, newBindings) {
 }
 
 export function bindData(config, env) {
-  return env[EnvInternals].bind(config, env);
+  return envInternals.get(env).bind(config, env);
 }
 
 export function triggerChange(env, key) {
-  env[EnvInternals].onChange?.(key);
+  envInternals.get(env).onChange?.(key);
 }
 
 function setInternals(env, internals) {
-  Object.defineProperty(env, EnvInternals, {
-    enumerable: false,
-    configurable: false,
-    get: () => internals,
-  });
+  if (!isEnv(env)) {
+    envInternals.set(env, internals);
+  }
 
   return env;
 }
