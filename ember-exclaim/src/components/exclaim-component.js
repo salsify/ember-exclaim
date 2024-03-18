0c5a1268558463672fea70a7feb1488958720b2e
Dan Freeman <dfreeman@salsify.com>
2024-03-18 18:35:35 +0000

    Don't rely on `@cached`

--- ember-exclaim/src/components/exclaim-component.js
+++ ember-exclaim/src/components/exclaim-component.js
@@ -1,13 +1,10 @@
 import Component from '@glimmer/component';
-import { cached } from '@glimmer/tracking';
 import { extendEnv } from '../-private/env/index.js';
 
 export default class ExclaimComponent extends Component {
-  @cached get env() {
-    return extendEnv(this.args.env, this.args.additionalEnvData);
-  }
-
-  get config() {
-    return this.args.componentSpec?.resolveConfig?.(this.env);
+  get componentData() {
+    let env = extendEnv(this.args.env, this.args.additionalEnvData);
+    let config = this.args.componentSpec?.resolveConfig?.(env);
+    return { env, config };
   }
 }
